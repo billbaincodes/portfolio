9fba829b5bf806b37e55c04e50f6ac8667adf436
bill <williamandrewbain@gmail.com>
2018-11-07 14:07:07 -0700

    Created react app, removed initial styling

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+
+    <footer className="footer">
+      <p>Copyright 2018</p>
+    </footer>
+
+    
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+    <nav className="header">
+      <p>Header</p>
+      <a className="title" href="index.html">Home</a>
+      <a className="title" href="index.html">Projects</a>
+      <a className="title" href="index.html">About</a>
+    </nav>
+
+    
+  )
+}
+
+export default Header
