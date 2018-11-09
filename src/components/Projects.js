86e0c3412e9fea6c192d40f4adeb95a648e53276
bill <williamandrewbain@gmail.com>
2018-11-09 13:32:15 -0700

    Started building modal component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,12 @@
 import React from 'react'
 
 
-const Projects = ({ modal, modalToggle }) => {
+const Projects = () => {
 
   return (
 
     <main className="main">
-      <button onClick={modalToggle}>Modal Time</button>
-      <div className={modal ? 'modal-display' : 'modal-hidden'}>
-        <div class="modal-content">
-          <span class="close" onClick={modalToggle}>&times;</span>
-          <p>Some text in the Modal.</p>
-          <a href="https://www.linkedin.com/in/billbaincodes/" target="blank"><img className="logo" alt="LinkedIn"></img></a>
-          <p>Github</p>
-        </div>
-      </div>
+      <p>My Projjies</p>
     </main>
 
   )
