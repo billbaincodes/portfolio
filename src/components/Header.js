fcd6201beea874d8412ec6ce310fd807d1e7bd82
bill <williamandrewbain@gmail.com>
2018-11-15 20:50:12 -0700

    Restyled nav link positions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Header = ({modalToggle, navigationSelect}) => {
+const Header = ({ modalToggle, navigationSelect }) => {
 
   return (
     <nav className="header">
       <h4 className="headerLogo">Bill Bain Codes</h4>
-      <button className="navLink" onClick={() => {navigationSelect('home')}}>Home</button>
-      <button className="navLink" onClick={() => {navigationSelect('projects')}}>Projects</button>
-      <button className="navLink" onClick={() => {navigationSelect('about')}}>About</button>
-      <button className="navLink" onClick={modalToggle}>Contact</button>
+        <section className="navBar">
+          <button className="navLink" onClick={() => {navigationSelect('home')}}>Home</button>
+          <button className="navLink" onClick={() => {navigationSelect('projects')}}>Projects</button>
+          <button className="navLink" onClick={() => {navigationSelect('about')}}>About</button>
+          <button className="navLink" onClick={modalToggle}>Contact</button>
+      </section>
     </nav>
   )
 }
